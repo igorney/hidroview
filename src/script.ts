0bd61497f9ace95f0f5df88ff94060d4b44e65d8
Igor <48994130+igorney@users.noreply.github.com>
2023-07-04 01:34:48 +0000

    feat: load ts

--- src/script.ts
+++ src/script.ts
@@ -1,10 +1,18 @@
-function loadContent(){
-  const buttons = document.querySelectorAll('btn-success')
-  if(buttons){
-    buttons.forEach(btn => {
-      btn.addEventListener('click', ev => {
-        const 
-      })
-    }
-  }
+function loadContent(url: string) {
+  fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.text()
+      }
+      throw new Error(response.status.toString())
+    })
+    .then(text => {
+      const node = document.querySelector('.btn-success')
+      if (node) {
+        node.textContent = text
+      }
+    })
+    .catch(reason => {
+      console.log('Failed to load, reason:' + reason)
+    })
 }
