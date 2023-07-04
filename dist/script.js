7e540f709658194211e1d13961ae32b5dcb452b2
Igor <48994130+igorney@users.noreply.github.com>
2023-07-04 01:34:22 +0000

    build: config lint and ts

---
+++ dist/script.js
@@ -0,0 +1,11 @@
+"use strict";
+function loadContent() {
+    const buttons = document.querySelectorAll('btn-success');
+    if (buttons) {
+        buttons.forEach(btn => {
+            btn.addEventListener('click', ev => {
+                const ;
+            });
+        });
+    }
+}
---
+++ src/script.ts
@@ -0,0 +1,10 @@
+function loadContent(){
+  const buttons = document.querySelectorAll('btn-success')
+  if(buttons){
+    buttons.forEach(btn => {
+      btn.addEventListener('click', ev => {
+        const 
+      })
+    }
+  }
+}
